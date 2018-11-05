c04fce8aae0c9fee45334202fa392177010d5cc7
LvChengbin <lvchengbin59@gmail.com>
2018-11-05 20:29:22 +0800

    removed node v7 from .travis.yml

--- test/error/index.spec.js
+++ test/error/index.spec.js
@@ -1,17 +1,28 @@
 const request = require( 'supertest' );
+const mockConsole = require( 'jest-mock-console' ).default;
 const app = require( './app' );
 
 require('max-listeners-exceeded-warning')();
 
 describe( 'error', () => {
 
+    let restoreConsole;
+
     beforeAll( () => app.ready() );
+
+    beforeAll( () => {
+        restoreConsole = mockConsole();
+    } );
+
+    afterAll( () => {
+        restoreConsole();
+    } );
 
     for( const item of [ 500, 502, 500, 403, 404 ] ) {
         it( `catch ${item}`, done => {
-        request( app.listen() ).get( `/index/e${item}` )
-            .expect( item )
-            .end( e => e ? done.fail( e ) : done() );
+            request( app.listen() ).get( `/index/e${item}` )
+                .expect( item )
+                .end( e => e ? done.fail( e ) : done() );
         } );
     }
 } );
