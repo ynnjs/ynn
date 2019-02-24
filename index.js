87bce3b22a95a1a9460eb7f43d570128d21105d0
LvChengbin <lvchengbin59@gmail.com>
2019-02-24 18:50:29 +0800

    fixed bugs of using multiple release in one project

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const YNN_GLOBAL_MOUNTING_DATA = Symbol.for( 'ynn#global#mounting#data' );
+global[ YNN_GLOBAL_MOUNTING_DATA ] = {
+    status : 0,
+    set( obj ) {
+        Object.assign( this, obj );
+        this.status = 1;
+    },
+    reset() {
+        this.status = 0;
+    }
+};
+
 module.exports = require( './lib/core' );
 module.exports.Base = require( './lib/base' );
 module.exports.Runtime = require( './lib/runtime' );
