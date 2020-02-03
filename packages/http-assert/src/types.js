870f3b1311212bab2284dcac03df23a263f2c1ff
LvChengbin <lvchengbin59@gmail.com>
2020-02-03 15:35:18 +0800

    start 1.0

---
+++ packages/http-assert/src/types.js
@@ -0,0 +1,10 @@
+module.exports = {
+    email : 'email',
+    int : 'integer',
+    integer : 'integer',
+    ip : 'ip',
+    ipv4 : 'ipv4',
+    ipv6 : 'ipv6',
+    number : 'number',
+    url : 'url'
+};
