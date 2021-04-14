10ca2fb27151e0ddcf2ab98b256d1b4bafcf9429
LvChengbin <lvchengbin59@gmail.com>
2021-04-14 08:19:18 +0800

    test: add required.spec.ts

---
+++ packages/pipes/test/required.spec.ts
@@ -0,0 +1,17 @@
+/******************************************************************
+ * Copyright (C) 2021 LvChengbin
+ *
+ * File: test/required.spec.ts
+ * Author: LvChengbin<lvchengbin59@gmail.com>
+ * Time: 04/13/2021
+ * Description:
+ ******************************************************************/
+
+import { HttpException } from '@ynn/core';
+import { Required } from '../src';
+
+describe( 'Required Pipe', () => {
+    it( '', () => {
+        expectRequire( null )
+    } ); 
+} );
